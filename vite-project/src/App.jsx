59a81d7988de76d0be2b078c80802e066113dd6d
Sandhiya R <sandhiya-077@gmail.com>
2025-02-18 13:31:02 +0530

    fourth assignment

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let name="Sandhiya";
+  console.log('My name is',name);
+  alert('Hello')
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
