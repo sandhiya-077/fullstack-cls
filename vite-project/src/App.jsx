31744c67ca0aabea7baa7b3dedb08076b213caf8
Sandhiya R <sandhiya-077@gmail.com>
2025-02-19 13:56:16 +0530

    assignment 5

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -18,18 +18,32 @@ function App() {
     SecId:"Sec23cj001",
     Rollno1:18,
   }
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
   return (
     <>
-    <div>
-    <h1 style={{textAlign:"center",color:"pink"}}>Friends forever</h1>
+    
+    <div className={isDarkMode ? "dark-theme" : "light-theme"}>
+      <div className="content">
+        <h1 style={{ textAlign: "center", color: isDarkMode ? "#F9F6E6" : "darkred" }}>
+          Friends Forever :)
+        </h1>
       <Comp1 name={details.name} Rollno={details.Rollno} />
       <Comp1 name1={info2.name1} Rollno1={info2.Rollno1}/>
       <h2 stlye={{textAlign:"center",color:"black"}}>My SecId is {details.SecId}</h2>
       <button onClick={()=>{alert('Onclick is clicked')}}>click</button>
       <button onDoubleClick={()=>{alert('On double click is clicked')}}>Double click </button>
-
-      
+      <button className="toggle-button" onClick={toggleTheme}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button>
     </div>
+  </div>
+  
+      
+ 
     </>
   )
 }
