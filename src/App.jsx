ba385489abd34d64daeaa790acb01287b1500025
Sandhiya R <sandhiya-077@gmail.com>
2025-02-22 20:23:01 +0530

    assignment 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App(){
 
 
   const [name,setName]=useState("");
-  const [age,setAge]=useState("");
+  const [age,setId]=useState("");
   const handleSubmit=(e)=>{
     e.preventDefault();
     console.log(name);
-    console.log(age);
+    console.log(id);
   }
 return (
   <>
@@ -17,7 +17,7 @@ return (
     <form className='form' onSubmit={handleSubmit}>
       <h1>Registration Form</h1>
       <input className='input' type="text" required value={name} placeholder="Enter your Name" onChange={(e)=>setName(e.target.value)}/>
-      <input className='input' type="text" required value={age} placeholder='Enter your Age' onChange={(e)=>setAge(e.target.value)}/>
+      <input className='input' type="text" required value={id} placeholder='Enter your Id' onChange={(e)=>setId(e.target.value)}/>
       <input className='email' type="emai" required placeholder='Enter your Email'/>
       <input className='password' type="password" required placeholder='Enter your Password'/>
       <input className='DOB' type="Date" required placeholder='Enter your date'/>
